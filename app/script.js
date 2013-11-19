b35558c97a1ea6b95e554bda064cbbdf211d4d35
Cosmin Malutan <cosmin.malutan@softvision.ro>
2013-11-19 18:09:15 +0200

    Takes locales from ftp

--- app/script.js
+++ app/script.js
@@ -22,9 +22,10 @@ mciconf.controller('mainController', ['$scope', '$rootScope', '$http',
   function mciconf($scope, $rootScope, $http) {
     $rootScope.builds = [];
     $rootScope.iconClasses = ["icon-question-sign", "icon-ok", "icon-remove"];
-    $rootScope.buttonClasses = ["btn-warning", "btn-success", "btn-danger"];
+    $rootScope.buttonClasses = ["btn-primary", "btn-success", "btn-warning"];
     $rootScope.locales = ["en-US"];
     $rootScope.ff_versions = [];
+    $rootScope.ff_versions_type = [];
     $rootScope.updateChannels = ["default", "release", "esr", "beta", "aurora", "nightly"];
     $rootScope.update_channel = "default";
 
@@ -34,8 +35,10 @@ mciconf.controller('mainController', ['$scope', '$rootScope', '$http',
         doc.innerHTML = res.data.split('<table>')[1].split('</table>')[0];
         doc.innerHTML = res.data.split('<table>')[1].split('</table>')[0];
         var elements = doc.getElementsByTagName(aTag);
-        for (var element in elements)
-          aCallbackFilter(elements[element]);
+        for (var element in elements) {
+          if (aCallbackFilter(elements[element]))
+            break;
+        }
         if (aFinalCallback)
           aFinalCallback();
       });
@@ -44,11 +47,10 @@ mciconf.controller('mainController', ['$scope', '$rootScope', '$http',
     //START Retrieving data
     $http.get('data/dashboards.json').then(function (res){
       $rootScope.dashboards = res.data.dashboards;
-      $rootScope.dashboardsbuildUrl = res.data.dashboardsbuildUrl;
+      $rootScope.dashboards_url = res.data.dashboards_url;
       $rootScope.dashboard = $scope.dashboards[0];
     });
-
-    $rootScope.parseAtAddress('http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/', 'a',
+    $rootScope.parseAtAddress('http://ftp.mozilla.org/pub/mozilla.org/firefox/candidates/', 'a',
       function (link) {
         if (link.innerText && link.innerText.indexOf('-candidates') !== -1) {
           $rootScope.ff_versions.push(link.innerText.split('-candidates/')[0]);
@@ -58,13 +60,26 @@ mciconf.controller('mainController', ['$scope', '$rootScope', '$http',
         $rootScope.ff_versions.sort(function(a,b){
           return parseInt(b)-parseInt(a);
         });
-        $rootScope.builds.forEach(function (build, buildIndex) {
-          build.firefox_versions.forEach(function (version, versionIndex) {
-            if (!version.name){
-              version.name = $rootScope.ff_versions[0];
-              $rootScope.$emit('versionChanged', {versionIndex: versionIndex,
-                                                  buildIndex: buildIndex});
+
+        $rootScope.parseAtAddress('http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/', 'a',
+        function (link) {
+          if (link.innerText){
+            var v = link.innerText.split('/')[0];
+            if($rootScope.ff_versions.indexOf(v) !== -1) {
+              $rootScope.ff_versions_type[$rootScope.ff_versions.indexOf(v)] = 'release';
             }
+          }
+        },
+        function () {
+          $rootScope.builds.forEach(function (build, buildIndex) {
+            build.firefox_versions.forEach(function (version, versionIndex) {
+              if (!version.name){
+                version.name = $rootScope.ff_versions[0];
+                version.type = 'release';
+                $rootScope.$emit('versionChanged', {versionIndex: versionIndex,
+                                                    buildIndex: buildIndex});
+              }
+            });
           });
         });
       });
@@ -92,9 +107,9 @@ mciconf.controller('mainController', ['$scope', '$rootScope', '$http',
       var build = {};
       build.exists = STATE.NOT_CHECKED;
       build.name = ($rootScope.ff_versions.length) ? $rootScope.ff_versions[0] : "";
+      build.type = $rootScope.ff_versions_type[0];
       build.locale = "";
-      build.buildNumber = "release";
-      build.buildNumbers = ["release"];
+      build.availableLocales = [];
       var newEmptyBuild = {};
       newEmptyBuild.firefox_versions = [build];
       newEmptyBuild.platform = $rootScope.platforms.filter(notAdded)[0];
@@ -120,6 +135,8 @@ mciconf.controller('mainController', ['$scope', '$rootScope', '$http',
           newVersion.added = true;
         }
       });
+      $rootScope.$emit('versionChanged', {versionIndex: 0,
+                                          buildIndex: ($rootScope.builds.length -1)});
     };
 
     $scope.checkAll = function () {
@@ -131,6 +148,12 @@ mciconf.controller('mainController', ['$scope', '$rootScope', '$http',
       $rootScope.dashboard = $rootScope.dashboards[0];
       $rootScope.target_build_id = "";
       $rootScope.update_channel = "";
+      $rootScope.platforms.forEach(function (platform) {
+        platform.versions.forEach(function (version) {
+          version.added = false
+        });
+        platform.added = false;
+      });
       $rootScope.builds = [];
     }
 
@@ -157,29 +180,66 @@ mciconf.directive('build', function () {
         var build = {};
         build.exists = STATE.NOT_CHECKED;
         build.name = ($rootScope.ff_versions.length) ? $rootScope.ff_versions[0] : "";
+        build.type = $rootScope.ff_versions_type[0];
+        build.availableLocales = [];
         $rootScope.builds[aIndex].firefox_versions.push(build);
         $scope.versionChanged($rootScope.builds[aIndex].firefox_versions.length - 1, aIndex);
       };
-
+      $scope.buildNumberChanged = function (aVersionIndex, aBuildIndex) {
+        var url = "http://ftp.mozilla.org/pub/mozilla.org/firefox/";
+        var version = $rootScope.builds[aBuildIndex].
+                                 firefox_versions[aVersionIndex].
+                                 name.split("#")[0];
+        var candidate = $rootScope.builds[aBuildIndex].
+                                   firefox_versions[aVersionIndex].
+                                   buildNumber !== 'release'
+        url += (candidate) ? "candidates/" : "releases/";
+        url += version + ((candidate) ? "-candidates/" + $rootScope.builds[aBuildIndex].
+                                                                    firefox_versions[aVersionIndex].
+                                                                    buildNumber + "/" : "/");
+        url += PLATFORM_FRAGMENTS[$rootScope.builds[aBuildIndex].platform_version.platform ||
+                                  $rootScope.builds[aBuildIndex].platform.platform] + "/";
+        $rootScope.builds[aBuildIndex].
+                   firefox_versions[aVersionIndex].
+                   availableLocales = [];
+        $scope.unCheck(aVersionIndex, aBuildIndex);
+        $rootScope.parseAtAddress(url, "a", function (link) {
+          if (link.href && link.href.indexOf(link.innerText) !== -1) {
+            $rootScope.builds[aBuildIndex].
+                       firefox_versions[aVersionIndex].
+                       availableLocales.push(link.innerText.split('/')[0]);
+          }
+        });
+      }
       $scope.checkBuild = function (aVersionIndex, aBuildIndex) {
         // If there is no version and locales then we have to invalidate the build and return early
         if ( !$rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].locale) {
-          $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].exists = STATE.NOT_FOUND;
+          $rootScope.builds[aBuildIndex].
+                     firefox_versions[aVersionIndex].
+                     exists = STATE.NOT_FOUND;
           $scope.$emit('notify', {type: 'error',
                                   message: 'A localization must be given'});
           return;
         }
 
-        var candidate = $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].buildNumber !== 'release';
+        var candidate = $rootScope.builds[aBuildIndex].
+                                   firefox_versions[aVersionIndex].
+                                   buildNumber !== 'release';
 
         var foundBuilds = 0;
-        var locales = $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].locale.split(" ");
+        var locales = $rootScope.builds[aBuildIndex].
+                                 firefox_versions[aVersionIndex].
+                                 locale.split(" ");
         var url = "http://ftp.mozilla.org/pub/mozilla.org/firefox/";
-        var version = $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].name.split("#")[0];
+        var version = $rootScope.builds[aBuildIndex].
+                                 firefox_versions[aVersionIndex].
+                                 name.split("#")[0];
         var file = "";
 
         url += (candidate) ? "candidates/" : "releases/";
-        url += version + ((candidate) ? "-candidates/" + $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].buildNumber + "/" : "/");
+        url += version + ((candidate) ? "-candidates/" + $rootScope.builds[aBuildIndex].
+                                                                    firefox_versions[aVersionIndex].
+                                                                    buildNumber + "/" : "/");
         url += PLATFORM_FRAGMENTS[$rootScope.builds[aBuildIndex].platform_version.platform ||
                                   $rootScope.builds[aBuildIndex].platform.platform] + "/";
 
@@ -201,53 +261,70 @@ mciconf.directive('build', function () {
             break;
         }
 
-        locales.forEach(function (locale) {
-          var localeFound = false;
+        locales.forEach(function (locale, index) {
           $rootScope.parseAtAddress(url + locale + "/", "a", function (link) {
             if (link.innerHTML && link.innerHTML.indexOf(file)) {
-              localeFound = true;
               foundBuilds += 1;
               if (foundBuilds === locales.length) {
-                $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].exists = STATE.FOUND;
+                $rootScope.builds[aBuildIndex].
+                           firefox_versions[aVersionIndex].
+                           exists = STATE.FOUND;
                 $scope.$emit('notify', {type: 'success',
                                         message: 'Builds "' + locales.join('"', "'") + '" exists.'});
               }
+              return true;
             }
-          },
-          function () {
-            if (!localeFound) {
-              $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].exists = STATE.NOT_FOUND;
+            return false;
+          }, function () {
+            // After the last locale check if enough locales where found
+            if (locales.length === index &&  foundBuilds !== locales.length) {
+              $rootScope.builds[aBuildIndex].
+                         firefox_versions[aVersionIndex].
+                         exists = STATE.NOT_FOUND;
               $scope.$emit('notify', {type: 'error',
-                                      message: 'No Build was found at ' + file});
+                                      message: 'Build was not found'});
             }
           });
         });
       };
 
       $scope.checkLocales = function (aVersionIndex, aBuildIndex) {
-        $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].exists = STATE.NOT_CHECKED;
+        $rootScope.builds[aBuildIndex].
+                   firefox_versions[aVersionIndex].
+                   exists = STATE.NOT_CHECKED;
         $timeout.cancel($scope.checker);
-        var locales = $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].locale.split(" ");
+        var locales = $rootScope.builds[aBuildIndex].
+                                 firefox_versions[aVersionIndex].
+                                 locale.split(" ");
+        var availableLocales = ($rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].availableLocales.length) ?
+                                $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].availableLocales :
+                                $rootScope.locales;
         $scope.checker = $timeout(function() {
+          var availableL = availableLocales.toString().toLowerCase().split(",");
           for (var i = 0; i < locales.length; i += 1) {
-            if ($rootScope.locales.indexOf(locales[i]) === -1) {
+            if (availableL.indexOf(locales[i].toLowerCase()) === -1) {
               $scope.$emit('notify', {type: 'error',
-                                      message: 'Locales "' + locales.join('", "') + '" were not found!'});
+                                      message: "Locale '" + locales[i] + "' it's not supported!"});
               locales.splice(i, 1);
               i -= 1; // If we remove one item the next one will be on the same index
+            } else {
+              locales[i] = availableLocales[availableL.indexOf(locales[i].toLowerCase())];
             }
           }
           $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].locale = locales.join(" ");
-          $scope.checkBuild(aVersionIndex, aBuildIndex);
         }, 3000, true);
       };
 
       $scope.unCheck = function (aVersionIndex, aBuildIndex) {
-        $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].exists = STATE.NOT_CHECKED;
+        $rootScope.builds[aBuildIndex].
+                   firefox_versions[aVersionIndex].
+                   exists = STATE.NOT_CHECKED;
       };
 
       $scope.updatePlatformVersion = function (aIndex) {
-        $rootScope.builds[aIndex].platform_version = $rootScope.builds[aIndex].platform.versions.filter(notAdded)[0];
+        $rootScope.builds[aIndex].
+                   platform_version = $rootScope.builds[aIndex].
+                                                 platform.versions.filter(notAdded)[0];
       };
 
       $scope.removeBuild = function(index) {
@@ -261,15 +338,30 @@ mciconf.directive('build', function () {
       };
 
       $scope.versionChanged = function (aVersionIndex, aBuildIndex) {
-        $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].buildNumbers = ['release'];
-        var version = $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].name + "-candidates/";
-        $rootScope.parseAtAddress("http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/" + version, 'a', function (link) {
+        var isRelease = $rootScope.ff_versions_type[$rootScope.ff_versions.
+                                                               indexOf($rootScope.builds[aBuildIndex].
+                                                                                  firefox_versions[aVersionIndex].
+                                                                                  name)];
+        $rootScope.builds[aBuildIndex].
+                   firefox_versions[aVersionIndex].
+                   buildNumbers = (isRelease) ? ['release'] : [];
+        var versionName = $rootScope.builds[aBuildIndex].
+                                 firefox_versions[aVersionIndex].
+                                 name + "-candidates/";
+        $rootScope.parseAtAddress("http://ftp.mozilla.org/pub/mozilla.org/firefox/candidates/" + versionName, 'a', function (link) {
           if (link.innerHTML && link.innerHTML.indexOf('build') !== -1) {
-            $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].buildNumbers.push(link.innerHTML.split('/')[0]);
+            $rootScope.builds[aBuildIndex].
+                       firefox_versions[aVersionIndex].
+                       buildNumbers.push(link.innerHTML.split('/')[0]);
           }
         },
         function () {
-          $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].buildNumber = $rootScope.builds[aBuildIndex].firefox_versions[aVersionIndex].buildNumbers[0];
+          $rootScope.builds[aBuildIndex].
+                     firefox_versions[aVersionIndex].
+                     buildNumber = $rootScope.builds[aBuildIndex].
+                                              firefox_versions[aVersionIndex].
+                                              buildNumbers[0];
+          $scope.buildNumberChanged(aVersionIndex, aBuildIndex);
         });
         $scope.unCheck(aVersionIndex, aBuildIndex);
       }
@@ -326,7 +418,7 @@ mciconf.directive('notification', function () {
   return {
     restrict: 'AE',
     templateUrl: 'templates/logger.html',
-    controller: function ($scope, $rootScope) {
+    controller: function ($scope, $rootScope, $timeout) {
       $scope.alerts = [];
       $scope.history = [];
       $scope.closeAlert = function (aIndex) {
@@ -338,6 +430,12 @@ mciconf.directive('notification', function () {
         $scope.alerts.reverse();
         if ($scope.alerts.length > 3)
           $scope.history.push($scope.alerts.pop());
+        $timeout(function () {
+          $scope.history.push($scope.alerts.pop());
+          if(!$scope.$$phase) {
+            $scope.$digest();
+          }
+        }, 3000, false);
       });
     }
   }
