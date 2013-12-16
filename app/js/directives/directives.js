811b4315d9bdf3977f2bd0311106487fc23ad0b4
Cosmin Malutan <cosmin.malutan@softvision.ro>
2013-12-16 14:04:08 +0200

    Save the config file - beta

--- app/js/directives/directives.js
+++ app/js/directives/directives.js
@@ -561,3 +561,19 @@ mciconf.directive('configPicker', function () {
     }
   };
 });
+mciconf.directive('configSaver', function () {
+
+  return {
+    restrict: 'AE',
+    templateUrl: 'templates/config-saver.html',
+    controller: function ($scope) {
+      $scope.save = function () {
+        var link = document.getElementById("config-saver");
+        var content = document.getElementsByTagName("pre")[0].textContent;
+        link.href = URL.createObjectURL(new Blob([content], {type: "text/plain"}));
+        link.download = "onDemandConfig.ini";
+        link.click();
+      }
+    }
+  }
+});
